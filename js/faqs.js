12eb8c9b9a8278a938b22af93391b98b98541da0
Daniel Saltzman <113066562+dansaltz@users.noreply.github.com>
2023-12-04 04:55:55 -0500

    Add files via upload

---
+++ js/faqs.js
@@ -0,0 +1,11 @@
+function interest(){
+    document.querySelector('#emailDiv').style.display="none";
+    if (document.getElementById('subscribe').checked==true){
+        document.getElementById('emailDiv').style.display = "block";
+    }else{
+        document.querySelector('#emailDiv').style.display="none";
+    }
+}
+function myFunction() {
+    alert("Thank you for your interest!");
+}
